7e0cc323e896d837ec14dc30fdef685c01600050
Emile Snyder <emile@teammobot.com>
2023-03-28 15:42:19 -0700

    Comment out setting the codec.options to see if that makes h264_v4l2m2m happier...

--- src/aiortc/codecs/h264.py
+++ src/aiortc/codecs/h264.py
@@ -131,11 +131,11 @@ def create_encoder_context(
     codec.pix_fmt = "yuv420p"
     codec.framerate = fractions.Fraction(MAX_FRAME_RATE, 1)
     codec.time_base = fractions.Fraction(1, MAX_FRAME_RATE)
-    codec.options = {
-        "profile": "baseline",
-        "level": "31",
-        "tune": "zerolatency",  # does nothing using h264_omx
-    }
+    # codec.options = {
+    #     "profile": "baseline",
+    #     "level": "31",
+    #     "tune": "zerolatency",  # does nothing using h264_omx
+    # }
     codec.open()
     return codec, codec_name == "h264_v4l2m2m"
 
